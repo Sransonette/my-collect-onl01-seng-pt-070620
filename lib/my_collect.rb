4a60aa06f656181ec6566cc21c173716eefc23f3
Sean Ransonette <sransonette@gmail.com>
2020-07-15 22:52:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ def my_collect(array)
   if block_given?
     i = 0
     while i < array.length
-    yield(array[i]).uppercase
+    yield(array[i]).up
     i += 1
   end
   end
-  languages
+  languages.uppercase
 end
